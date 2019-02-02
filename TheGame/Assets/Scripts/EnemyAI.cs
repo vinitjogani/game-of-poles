36bbebb575a9755981760da1f85263f971fa338e
Ge0rges <ge0rges@ge0rges.com>
2019-02-02 15:00:35 -0500

    Added bullet to enemy. Added health bar to enemy.

--- TheGame/Assets/Scripts/EnemyAI.cs
+++ TheGame/Assets/Scripts/EnemyAI.cs
@@ -11,9 +11,21 @@ public class EnemyAI : MonoBehaviour
     public float maximumAttackDistance = 10f;
     public float rotationDamping = 2f;
     public float shotInterval = 0.5f;
-    public float health = 100f;
- 
+    public float health = 100f; // Current health
+    public float maxHealth = 100f; // Used for health bar
+
+    public Texture healthbarBackground;
+    public Texture healthbarForeground;
+
     private float shotTime = 0f;
+
+    private void Start()
+    {
+        if (System.Math.Abs(health - maxHealth) > 0)
+        {
+            Debug.Log("Warning EnemyAI has maxHealth != health (curr) at start.");
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -64,5 +76,24 @@ public class EnemyAI : MonoBehaviour
         Instantiate(projectile,
             transform.position + (target.transform.position - transform.position).normalized,
             Quaternion.LookRotation(target.transform.position - transform.position));
+    }
+
+    // The bullet will call this.
+    void TakeDamage(float damage)
+    {
+        this.health -= damage;
+    }
+
+    // Display a health bar
+    void OnGUI()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        // Draw Background
+        GUI.DrawTexture(new Rect(screenPoint.x - 100, screenPoint.y - 20, 200, 40),
+                        healthbarBackground, ScaleMode.StretchToFill, true);
+        // Draw Foreground
+        GUI.DrawTexture(new Rect(screenPoint.x - 100, screenPoint.y - 20, 200F * health/maxHealth, 40),
+                        healthbarForeground, ScaleMode.StretchToFill, true,
+                        ((healthbarForeground.width * health) / healthbarForeground.height));
     }
 }

0f7629007134c9428ba990b5014ae96299529bc7
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-02-03 23:24:58 -0500

    Arrow Shooting

---
+++ TheGame/Assets/Scripts/Arrow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    Rigidbody rb;
+    private float lifeTime = 2f;
+    private float timer;
+    private bool hit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.rotation = Quaternion.LookRotation(rb.velocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer>= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+
+        if (!hit)
+        {
+            transform.rotation = Quaternion.LookRotation(rb.velocity);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag != "Arrow" && collision.collider.tag != "Player")
+        {
+            hit = true;
+            Stick();
+        }
+    }
+
+    private void Stick()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+}
---
+++ TheGame/Assets/Scripts/Player/ArrowShooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShooter : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetMouseButtonDown(0))
+        {
+            GameObject newArrow = Instantiate(arrowPrefab) as GameObject;
+            newArrow.transform.position = transform.position;
+            Rigidbody rb = newArrow.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward*20;
+        }
+    }
+}

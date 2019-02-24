c9d2ce8c317b1ff610dd3b0f2ba7c06132f74bda
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-02-23 23:27:16 -0500

    Button Activator

---
+++ TheGame/Assets/Scripts/ButtonActivator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivator : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        gameObject.AddComponent<Rigidbody>();
+        GetComponent<AudioSource>().Play();
+    }
+}

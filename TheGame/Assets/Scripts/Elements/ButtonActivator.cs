06fc5d584fc623b83c585ded589f719531372c45
Vinit Jogani <vnjogani@gmail.com>
2019-02-27 23:51:04 -0500

    Organized heirarchy and added ambient track

---
+++ TheGame/Assets/Scripts/Elements/ButtonActivator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivator : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        var body = gameObject.AddComponent<Rigidbody>();
+        body.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        body.velocity = Vector3.down * 5f;
+        GetComponent<AudioSource>().Play();
+    }
+}
---
+++ TheGame/Assets/Scripts/UI/UIInteract.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInteract : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var minAngle = 360f;
+        GameObject minLevel = null;
+        foreach (var level in GameObject.FindGameObjectsWithTag("Button"))
+        {
+            Vector3 dir = level.transform.position - Camera.main.transform.position;
+            var angle = Vector3.Angle(dir, Camera.main.transform.forward);
+            if (angle < minAngle)
+            {
+                minAngle = angle;
+                minLevel = level;
+            }
+        }
+
+        if (minLevel != null)
+            minLevel.GetComponent<Button>().Select();
+
+        if (Input.GetAxis("Axis9") > 0 || Input.GetKey(KeyCode.E))
+        {
+            if (minLevel.name == "Quit")
+                Application.Quit();
+            else
+                Application.LoadLevel(minLevel.name);
+        }
+    }
+}

7cfdf04284fc8b955c3d0482e0733975f4e733a5
Geetchopra <geet.s.chopra@gmail.com>
2019-03-06 18:50:03 -0500

    Added tutorial

---
+++ TheGame/Assets/OpenDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject plane;
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        Destroy(plane);
+
+    }
+}

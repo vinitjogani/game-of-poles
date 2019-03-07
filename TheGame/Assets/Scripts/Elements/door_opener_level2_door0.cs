e637d13e92d5a0e8ab047f000afcd7410b59caf8
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-03-07 04:51:44 -0500

    Level 2

---
+++ TheGame/Assets/Scripts/Elements/door_opener_level2_door0.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_opener_level2_door0 : MonoBehaviour
+{
+    Animator anim;
+    BoxCollider boxColl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        boxColl = GetComponent<BoxCollider>();
+    }
+
+  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        anim.SetTrigger("character_nearby");
+        // Play door sound
+        AudioSource laudio = gameObject.AddComponent<AudioSource>();
+        laudio.PlayOneShot((AudioClip)Resources.Load("fallingWall"));
+        boxColl.enabled = false;
+    }
+}
---
+++ TheGame/Assets/Scripts/Elements/door_opener_level2_door1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door_opener_level2_door1 : MonoBehaviour
+{
+    Animator animat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animat = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animat.SetTrigger("character_nearby");
+            AudioSource laudio = gameObject.AddComponent<AudioSource>();
+            laudio.PlayOneShot((AudioClip)Resources.Load("fallingWall"));
+        }
+
+    }
+
+
+}

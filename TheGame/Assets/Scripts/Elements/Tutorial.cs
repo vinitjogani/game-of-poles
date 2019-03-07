12eef1defad824fba807acf1d8b641120a20fa03
Geetchopra <geet.s.chopra@gmail.com>
2019-03-07 01:19:10 -0500

    Fixed Tutorial

---
+++ TheGame/Assets/Scripts/Elements/Tutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject moveplane;
+    public GameObject shootplane;
+    public GameObject attractplane;
+    public int objective;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name == "Ball(Clone)" && objective == 1)
+        {
+            Debug.Log("Ball hit");
+            Destroy(shootplane);
+            Destroy(collider);
+        }
+        else if (collider.name == "Ball(Clone)" && objective == 0)
+        {
+            
+            Destroy(moveplane);
+            Destroy(collider);
+        }
+        else if (collider.name == "Player" && objective == 0) {
+            Destroy(moveplane);
+        }
+        else if (collider.name == "AttractCube" && objective == 3)
+        {
+            Destroy(attractplane);
+        }
+    }
+}

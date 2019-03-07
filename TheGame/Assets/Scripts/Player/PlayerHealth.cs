6a3d03ba447641187b768c51d8da6da7b6e50dbb
Vinit Jogani <vnjogani@gmail.com>
2019-03-07 13:27:39 -0500

    Added audio voiceover

--- TheGame/Assets/Scripts/Player/PlayerHealth.cs
+++ TheGame/Assets/Scripts/Player/PlayerHealth.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 public static class PlayerState {
     public static Vector3? respwanAt = null;
     public static Quaternion? rotation = null;
-
     public static int level = -1;
+
 }
 
 public class PlayerHealth : MonoBehaviour
@@ -16,6 +16,9 @@ public class PlayerHealth : MonoBehaviour
 
     private Texture2D black;
     private float health;
+    private AudioSource lAudio;
+
+    private bool restartLevel = false;
 
     private void Start()
     {
@@ -23,6 +26,9 @@ public class PlayerHealth : MonoBehaviour
         {
             transform.position = PlayerState.respwanAt.Value;
             Camera.main.transform.rotation = PlayerState.rotation.Value;
+
+            lAudio = gameObject.AddComponent<AudioSource>();
+            lAudio.PlayOneShot((AudioClip)Resources.Load("respawn"));
         }
 
         PlayerState.level = SceneManager.GetActiveScene().buildIndex;
@@ -35,27 +41,33 @@ public class PlayerHealth : MonoBehaviour
 
     private void Update()
     {
+        if (restartLevel && !lAudio.isPlaying)
+        {
+            SceneManager.LoadScene(PlayerState.level);
+        }
+
         PlayerState.respwanAt = transform.position;
         PlayerState.rotation = Camera.main.transform.rotation;
     }
 
     public void TakeDamage(float damage)
     {
+        if (health > 15 && health - damage < 15)
+        {
+            lAudio = gameObject.AddComponent<AudioSource>();
+            lAudio.PlayOneShot((AudioClip)Resources.Load("ohDeath"));
+        }
+
         health -= damage;
         black.SetPixel(0, 0, new Color(0, 0, 0, 1 - health / maxHealth));
         black.Apply();
 
-        if (health <= 0) RestartLevel();
+        if (health <= 0) restartLevel = true;
     }
 
 
     void OnGUI()
     {         
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), black);
-    }
-
-    void RestartLevel()
-    {
-        SceneManager.LoadScene(PlayerState.level);
     }
 }

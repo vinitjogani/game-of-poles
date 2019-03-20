f89691ab720ebc6e3213b57b8d4260ead6bf7e48
Vinit Jogani <vnjogani@gmail.com>
2019-03-20 15:07:56 -0400

    Added mouselook prefab

---
+++ TheGame/Assets/Scripts/MouseLock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+    CursorLockMode wantedMode;
+
+    private void Start()
+    {
+        SwitchLockState();
+    }
+
+    // Apply requested cursor state
+    void SwitchLockState()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) SwitchLockState();
+    }
+}

ab579867f3b4923a6ce7a7cc82bcf4e36a067cd4
Geetchopra <geet.s.chopra@gmail.com>
2019-03-30 21:23:19 -0400

    Updated level 4 and high score screen

---
+++ TheGame/Assets/Scripts/UI/LoadHighScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadHighScore : MonoBehaviour
+{
+    public Text highScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string scoreString = (WaveSpawner.highScore).ToString();
+        while (scoreString.Length < 4)
+            scoreString = "0" + scoreString;
+        highScoreText.text = "HIGH SCORE\n" + scoreString;
+    }
+
+}
